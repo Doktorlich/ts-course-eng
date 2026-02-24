cd79f038ada6a5bd8a765e816e9e4b8f4d164320
Doktorlich <sergei199522@mail.ru>
2026-02-24 10:24:40 +0300

    09. Classes & Generics - Demo Project

---
+++ 09-classes-and-generics-demo-project/01-project/linked-list.ts
@@ -0,0 +1,28 @@
+class ListNode<T> {
+    next?: ListNode<T>;
+    constructor(public value: T) {}
+}
+
+class LinkedList<T> {
+    private root?: ListNode<T>;
+    private tail?: ListNode<T>;
+    private length = 0;
+
+    add(value: T) {
+        const node = new ListNode(value);
+        if (!this.root) {
+            this.root = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+            // let current = this.root;
+            // while (current.next) {
+            //     current = current.next;
+            // }
+            // current.next = node;
+        }
+        this.length++;
+    }
+}
+const list = new LinkedList();
